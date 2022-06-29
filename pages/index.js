0d46f4255db160f3ea7dc3408141c151eda7f0fb
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-06-29 21:25:27 +0100

    testing CI deploy with Netlify

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ const Index = (/*{ productsServ }*/) => {
       <div className={styles.indexDiv}>
         <div className={styles.indexImgDiv}>
           <img style={{ width: "100%" }} src="/goods.jpg"></img>
-          <p style={{ color: "red" }}>Test CI again4</p>
+          <p style={{ color: "red" }}>Test CI again1</p>
         </div>
 
         <div className={styles.indexTextDiv}>
